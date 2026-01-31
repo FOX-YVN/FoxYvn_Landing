cc0aff596369347a37999f53fe533d4c4701ad5b
E3FE3 <229452262+E3FE3@users.noreply.github.com>
2026-01-31 10:34:56 -0800

    Delete project directory

--- project/client/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface UmamiTracker {
-  track: (event: string, payload?: Record<string, unknown>) => void;
-}
-
-interface Window {
-  umami?: UmamiTracker;
-}
